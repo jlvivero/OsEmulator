a94e3a1ac292ce3039ed3e1acf5345fe455955e9
jlvivero <josluivivgar@gmail.com>
2015-11-25 17:10:40 -0800

    added some ui parts of the memory management module

---
+++ Assets/hideMe.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hideMe : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    public void hideThis(bool onOff)
+    {
+        gameObject.SetActive(onOff);
+    }
+
+    public void hideThat(bool onOff)
+    {
+        gameObject.SetActive(!onOff);
+    }
+}
